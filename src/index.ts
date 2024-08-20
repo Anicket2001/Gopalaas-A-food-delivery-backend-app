1f6917a31b9491f919a3260d9d08e69bb3682d15
Anicket2001 <anicketshardha@gmail.com>
2024-08-20 16:40:20 +0530

    adding profile and admin feature Second Commit

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import MyUserRoute from "./routes/MyUserRoute";
+import {v2 as cloudinary} from 'cloudinary';
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
+// import restaurantRoute from "./routes/RestaurantRoute";
+// import orderRoute from "./routes/OrderRoute";
+
 const PORT = process.env.PORT || 8000;
 
 
@@ -13,10 +18,18 @@ mongoose
 .then(() => console.log("MongoDB DataBase Connected"))
 .catch((err) => console.log(`Error occured during connection with database ${err}`));
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
 
 // app.get('/test', async (req: Request, res: Response) => {
 //     res.json({message: "Hare Krishna"});
@@ -25,7 +38,12 @@ app.use(cors());
 app.get("/health", async (req: Request, res: Response) => {
     res.send({ message: "health OK!" });
 });
+
+
 app.use("/api/my/user", MyUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
+// app.use("/api/restaurant", restaurantRoute);
+// app.use("/api/order", orderRoute);
 
 
 app.listen(PORT, () => console.log(`Server is listening on Port: ${PORT}`)
